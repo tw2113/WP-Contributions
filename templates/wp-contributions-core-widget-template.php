b5e127f4d53c02b6ba20e2d6eeb8cc0690be09aa
Damon Cook <damonacook@gmail.com>
2015-05-06 15:47:54 -0400

    code formatting

--- templates/wp-contributions-core-widget-template.php
+++ templates/wp-contributions-core-widget-template.php
@@ -7,19 +7,22 @@
 ?>
 
 <?php if ( isset( $items ) ) : ?>
+
 		<ul>
-		<?php foreach ( (array) $items as $item ) : ?>
-			<?php if ( $item['ticket'] ) { ?>
-			<li><?php printf( __( '[%1$s] for %2$s' ),
-				'<a href="' . esc_url( $item['link'] ) . '">' . esc_html( $item['changeset'] ) . '</a>',
-				'<a href="' . esc_url( 'http://core.trac.wordpress.org/ticket/' . $item['ticket'] ) . '">' . esc_html( '#' . $item['ticket'] ) . '</a>'
-			); ?></li>
-			<?php } else { ?>
-			<li><?php printf( __( '[%1$s]' ),
-				'<a href="' . esc_url( $item['link'] ) . '">' . esc_html( $item['changeset'] ) . '</a>'
-			); ?></li>
-			<?php } ?>
-		<?php endforeach; ?>
+			
+			<?php foreach ( (array) $items as $item ) : ?>
+				<?php if ( $item['ticket'] ) { ?>
+				<li><?php printf( __( '[%1$s] for %2$s' ),
+					'<a href="' . esc_url( $item['link'] ) . '">' . esc_html( $item['changeset'] ) . '</a>',
+					'<a href="' . esc_url( 'http://core.trac.wordpress.org/ticket/' . $item['ticket'] ) . '">' . esc_html( '#' . $item['ticket'] ) . '</a>'
+				); ?></li>
+				<?php } else { ?>
+				<li><?php printf( __( '[%1$s]' ),
+					'<a href="' . esc_url( $item['link'] ) . '">' . esc_html( $item['changeset'] ) . '</a>'
+				); ?></li>
+				<?php } ?>
+			<?php endforeach; ?>
+		
 		</ul>
 		
 		<p>
