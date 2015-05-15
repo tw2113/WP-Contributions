57591aeee7b5a1aed2f686609df981d52dc2b6b5
Damon Cook <damonacook@gmail.com>
2015-05-14 20:06:07 -0400

    temporary placeholder markup for user badges

---
+++ templates/wp-contributions-user-badges-template.php
@@ -0,0 +1,91 @@
+<?php
+/**
+ * User Badges Template
+ */
+
+// Exit if accessed directly
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+?>
+
+<div class="wp-contributions user-badges">
+	
+	<ul class="item-list">
+		<li>
+			<span class="screen-reader-text">Accessibility Team</span>
+			<div class="badge item dashicons badge-accessibility dashicons-universal-access" title="Accessibility Team"></div>
+		</li>
+
+		<li>
+			<span class="screen-reader-text">Community Team</span>
+			<div class="badge item dashicons badge-community dashicons-groups" title="Community Team"></div>
+		</li>
+		
+		<li>
+			<span class="screen-reader-text">Core Team</span>
+			<div class="badge item dashicons badge-code-committer dashicons-editor-code" title="Core Team"></div>
+		</li>
+		
+		<li>
+			<span class="screen-reader-text">Documentation Team</span>
+			<div class="badge item dashicons badge-documentation dashicons-admin-page" title="Documentation Team"></div>
+		</li>
+		
+		<li>
+			<span class="screen-reader-text">Meta Team</span>
+			<div class="badge item dashicons badge-meta dashicons-networking" title="Meta Team"></div>
+		</li>
+		
+		<li>
+			<span class="screen-reader-text">Mobile Team</span>
+			<div class="badge item dashicons badge-mobile dashicons-smartphone" title="Mobile Team"></div>
+		</li>
+		
+		<li>
+			<span class="screen-reader-text">Plugin Developer</span>
+			<div class="badge item dashicons badge-plugins dashicons-admin-plugins" title="Plugin Developer"></div>
+		</li>
+		
+		<li>
+			<span class="screen-reader-text">Plugin Review Team</span>
+			<div class="badge item dashicons badge-plugins-reviewer  dashicons-admin-plugins" title="Plugin Review Team"></div>
+		</li>
+		
+		<li>
+			<span class="screen-reader-text">Support Team</span>
+			<div class="badge item dashicons badge-support dashicons-format-chat" title="Support Team"></div>
+		</li>
+		
+		<li>
+			<span class="screen-reader-text">Theme Developer</span>
+			<div class="badge item dashicons badge-themes dashicons-admin-appearance" title="Theme Developer"></div>
+		</li>
+		
+		<li>
+			<span class="screen-reader-text">Theme Review Team</span>
+			<div class="badge item dashicons badge-themes-reviewer dashicons-admin-appearance" title="Theme Review Team"></div>
+		</li>
+		
+		<li>
+			<span class="screen-reader-text">Training Team</span>
+			<div class="badge item dashicons badge-training-team dashicons-welcome-learn-more" title="Training Team"></div>
+		</li>
+		
+		<li>
+			<span class="screen-reader-text">Translation Contributor</span>
+			<div class="badge item dashicons badge-translation-contributor dashicons-translation" title="Translation Contributor"></div>
+		</li>
+		
+		<li>
+			<span class="screen-reader-text">Translation Editor</span>
+			<div class="badge item dashicons badge-translation-editor dashicons-translation" title="Translation Editor"></div>
+		</li>
+		
+		<li>
+			<span class="screen-reader-text">WordCamp Speaker</span>
+			<div class="badge item dashicons badge-speaker dashicons-megaphone" title="WordCamp Speaker"></div>
+		</li>
+	</ul>
+	
+</div><!-- .wp-contributions .user-badges -->
