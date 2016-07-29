655f2820d6ea8d9d416113889198457929ec11d8
Damon Cook <damonacook@gmail.com>
2016-07-29 16:31:48 -0400

    add new Plugin Card and Theme Card shortcodes

---
+++ inc/shortcodes.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ * Register WP Contributions shortcodes
+ *
+ * @package WP Contributions
+ */
+
+// Exit if accessed directly
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+/**
+ * Displays the HTML output of a Plugin Card.
+ *
+ * @param string $atts The slug of the plugin on WordPress.org.
+ */
+function wp_contributions_plugin_card_shortcode( $atts ) {
+
+	if ( ! $atts ) {
+		echo '<p class="wp-contributions-message alert">' . esc_html__( 'Please enter a plugin slug to display this shortcode, e.g. [wp_contributions_plugin_card slug="your-slug"]', 'wp-contributions' ) . '</p>';
+		return;
+	}
+
+	$args = shortcode_atts( array(
+		'slug' => $atts,
+		'type' => 'plugin',
+	), $atts ); // $atts['slug'] aka [wp_contributions_plugin_card type="plugin"]
+
+	wp_contributions_plugin_card( $args['slug'] );
+
+}
+add_shortcode( 'wp_contributions_plugin_card', 'wp_contributions_plugin_card_shortcode' );
+
+/**
+ * Displays the HTML output of a Theme Card.
+ *
+ * @param string $atts The slug of the theme on WordPress.org.
+ */
+function wp_contributions_theme_card_shortcode( $atts ) {
+
+	if ( ! $atts ) {
+		echo '<p class="wp-contributions-message alert">' . esc_html__( 'Please enter a theme slug to display this shortcode, e.g. [wp_contributions_theme_card slug="your-slug"]', 'wp-contributions' ) . '</p>';
+		return;
+	}
+
+	$args = shortcode_atts( array(
+		'slug' => $atts,
+		'type' => 'plugin',
+	), $atts ); // $atts['slug'] aka [wp_contributions_plugin_card type="plugin"]
+
+	wp_contributions_theme_card( $args['slug'] );
+
+}
+add_shortcode( 'wp_contributions_theme_card', 'wp_contributions_theme_card_shortcode' );
