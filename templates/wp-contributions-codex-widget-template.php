2c42296a9af4f8a4b3073f5c5f8775374804723f
Damon Cook <damonacook@gmail.com>
2016-09-30 12:35:39 -0400

    PHPCS cleanup and sanitization in Codex widget template

--- templates/wp-contributions-codex-widget-template.php
+++ templates/wp-contributions-codex-widget-template.php
@@ -1,5 +1,3 @@
-
-<?php if ( isset( $items ) ) : ?>
 <?php
 /**
  * Codex Widget Template.
@@ -9,31 +7,29 @@
  */
 
 ?>
+
+<?php if ( isset( $items ) ) : ?>
 	<div class="wp-contributions-codex codex-card card <?php echo esc_attr( $user ); ?>">
 		<ul>
 		<?php foreach ( (array) $items as $item ) :
 			$link = 'http://codex.wordpress.org/index.php?title=' . $item['title'] . '&oldid=' . $item['revision'];
-	
-			if ( (bool)$item['function_ref'] ) {
+
+			if ( ( bool ) $item['function_ref'] ) {
 		?>
-			<li><?php printf( __( 'Function: %1$s', 'wp-contributions' ),
-				'<a href="' . esc_url( $link ) . '" title="' . esc_html( $item['description'] ) . '">' . esc_html( $item['title'] ) . '</a>'
-			); ?></li>
+			<li><?php printf( esc_html( 'Function: %1$s', 'wp-contributions' ), '<a href="' . esc_url( $link ) . '" title="' . esc_html( $item['description'] ) . '">' . esc_html( $item['title'] ) . '</a>' ); ?></li>
 		<?php } else { ?>
-			<li><?php printf( __( 'For %1$s', 'wp-contributions' ),
-				'<a href="' . esc_url( $link ) . '" title="' . esc_html( $item['description'] ) . '">' . esc_html( $item['title'] ) . '</a>'
-			); ?></li>
+			<li><?php printf( esc_html( 'For %1$s', 'wp-contributions' ), '<a href="' . esc_url( $link ) . '" title="' . esc_html( $item['description'] ) . '">' . esc_html( $item['title'] ) . '</a>' ); ?></li>
 		<?php } ?>
 		<?php endforeach; ?>
 		</ul>
 		<p>
 			<a href="<?php echo esc_url( 'http://codex.wordpress.org/Special:Contributions/' . ucfirst( $user ) ); ?>">
 				<?php
-					if ( $total == 2 ) {
-						_e( "View both changes in the Codex.", 'wp-contributions' );
-					} else {
-						printf( _n( "View the change in the Codex.", "View all %d changes in the Codex.", $total, 'wp-contributions' ), $total );
-					}
+				if ( 2 == $total ) {
+					esc_html_e( 'View both changes in the Codex.', 'wp-contributions' );
+				} else {
+					printf( esc_html_e( 'View the change in the Codex.', 'View all %d changes in the Codex.', $total, 'wp-contributions' ), esc_html( $total ) );
+				}
 				?>
 			</a>
 		</p>
