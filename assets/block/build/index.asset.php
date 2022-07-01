8a73cddb1606abded7e2ea5a370716e9a966a46c
Emilse Ormeno <104102110+emilse-webdev@users.noreply.github.com>
2022-07-01 20:53:35 -0300

    Feature 33 - block libs upgrade (#34)
    Create a WP-Contributors block. Many props to @emilse-webdev for much of the initial work and creation and putting up with my nitpicky detail requests.

---
+++ assets/block/build/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-primitives', 'wp-server-side-render'), 'version' => 'aa98e6967839985ec5fb');
