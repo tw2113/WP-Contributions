e5166fa01e41b269cb62beb6b7ed32fb6f4202d8
Michael Beckwith <tw2113@gmail.com>
2022-07-01 18:41:34 -0500

    touch up styling a bit to make it a bit more consistent

--- assets/block/build/index.asset.php
+++ assets/block/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-primitives', 'wp-server-side-render'), 'version' => '2f1d2f688ea2adee7d3f');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-primitives', 'wp-server-side-render'), 'version' => 'aa98e6967839985ec5fb');
