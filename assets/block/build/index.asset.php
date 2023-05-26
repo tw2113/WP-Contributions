5bc5dad3b50a7f63ab61a558a25cb10b0046c99b
Michael Beckwith <tw2113@gmail.com>
2023-05-26 14:02:43 -0500

    update build files

--- assets/block/build/index.asset.php
+++ assets/block/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-primitives', 'wp-server-side-render'), 'version' => 'aa98e6967839985ec5fb');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-primitives', 'wp-server-side-render'), 'version' => 'aefcbf75d28ea50c48d5');
